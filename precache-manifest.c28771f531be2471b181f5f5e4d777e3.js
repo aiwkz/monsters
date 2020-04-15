a20162c125a9109cffdbcb4f20109d84922c4fb9
Fede <federico.pp@webstarted.com>
2020-04-15 19:39:07 -0300

    Updates

---
+++ precache-manifest.c28771f531be2471b181f5f5e4d777e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf773f4609fc1f2ec371b7e9d8e41d8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c616c31926094dbc767b",
+    "url": "/monsters/static/css/main.6a470a0a.chunk.css"
+  },
+  {
+    "revision": "e6258cbbe686f7ab5cff",
+    "url": "/monsters/static/js/2.9bf61b77.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.9bf61b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c616c31926094dbc767b",
+    "url": "/monsters/static/js/main.baefc540.chunk.js"
+  },
+  {
+    "revision": "8eb099a4c348945aa682",
+    "url": "/monsters/static/js/runtime-main.3e7da30a.js"
+  }
+]);
